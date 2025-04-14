81ddba81f3c88c429d09d54e50a0bf6d8cdaf7d2
StephanyAliyah <162716178+StephanyAliyah@users.noreply.github.com>
2025-04-14 00:50:42 -0300

    Update MainActivity.java

--- Codigo-Fonte/java/MainActivity.java
+++ Codigo-Fonte/java/MainActivity.java
@@ -1,1 +1,41 @@
+package br.com.temdetudo;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnIniciarCadastro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Referência do botão
+        btnIniciarCadastro = findViewById(R.id.btnIniciarCadastro);
+
+        // Clique no botão leva para a tela de cadastro
+        btnIniciarCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Ajuste da margem para respeitar a área da tela (barra de status, etc.)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
