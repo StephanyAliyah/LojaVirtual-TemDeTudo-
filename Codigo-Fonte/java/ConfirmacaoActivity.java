2f2786522b20f5c4b4219c69e3e80f0a1b004df7
StephanyAliyah <162716178+StephanyAliyah@users.noreply.github.com>
2025-04-14 00:53:23 -0300

    Update ConfirmacaoActivity.java

--- Codigo-Fonte/java/ConfirmacaoActivity.java
+++ Codigo-Fonte/java/ConfirmacaoActivity.java
@@ -1,1 +1,35 @@
+package br.com.temdetudo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ConfirmacaoActivity extends AppCompatActivity {
+
+    TextView textViewNome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirmacao);
+
+        textViewNome = findViewById(R.id.textViewNome);
+
+        // Recebe o nome enviado pela tela anterior
+        Intent intent = getIntent();
+        String nome = intent.getStringExtra("nomeUsuario");
+
+        // Exibe o nome (sem texto adicional)
+        textViewNome.setText(nome);
+    }
+
+    // Quando o botão "Fechar" for clicado, volta para a MainActivity
+    public void fechar(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
 
